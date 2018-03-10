ba953eb8132e99bdc61efc7aac290b6512c98ea7
Steven Torres <stevetorres7562@gmail.com>
2018-03-10 00:11:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,4 +16,12 @@ while (i<facts.length) {
   return array; 
 } 
 
-function 
+function iLoveTheBeatles (n) { 
+ var array = [];
+ do { array.push("I love the Beatles!");
+ n++ }  
+ 
+ while (n<15); 
+ return array; 
+ } 
+ 
