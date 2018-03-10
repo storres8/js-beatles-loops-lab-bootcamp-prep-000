aa16860aaf0377f4e171abd46d0c84e1429c8682
Steven Torres <stevetorres7562@gmail.com>
2018-03-10 00:07:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ function johnLennonFacts (facts){
 var array = [];
 var i = 0;
 while (i<facts.length) { 
-  array.push(`${facts[i]}!!!`)} 
-  i++
+  array.push(`${facts[i]}!!!`); 
+  i++;
+} 
   return array; 
 } 
 
